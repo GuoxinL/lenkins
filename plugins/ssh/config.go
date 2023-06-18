cdf47ad03892300cbff3e13bc2c51c44d88dcd15
Guoxin <lgx31@sina.cn>
2023-06-18 12:24:17 +0800

    optimize code return err

--- plugins/ssh/config.go
+++ plugins/ssh/config.go
@@ -57,7 +57,7 @@ func (s *Server) Replace(key, value string) {
 	s.PrivateKeyPathAuth = plugins.Replace(s.PrivateKeyPathAuth, key, value)
 }
 
-func (s *Server) GetConfig() *gossh.ClientConfig {
+func (s *Server) GetConfig() (*gossh.ClientConfig, error) {
 	config := &gossh.ClientConfig{
 		Timeout:         time.Second, //ssh 连接time out 时间一秒钟, 如果ssh验证错误 会在一秒内返回
 		User:            s.User,
@@ -67,44 +67,62 @@ func (s *Server) GetConfig() *gossh.ClientConfig {
 		config.Auth = []gossh.AuthMethod{gossh.Password(s.Password)}
 	}
 	if len(s.PrivateKey) != 0 {
-		config.Auth = []gossh.AuthMethod{PublicKeyAuthFunc(s.PrivateKey)}
+		authFunc, err := PublicKeyAuthFunc(s.PrivateKey)
+		if err != nil {
+			return nil, err
+		}
+		config.Auth = []gossh.AuthMethod{authFunc}
 	}
 	if len(s.PrivateKeyPathAuth) != 0 {
-		config.Auth = []gossh.AuthMethod{PublicKeyPathAuthFunc(s.PrivateKeyPathAuth)}
+		authFunc, err := PublicKeyPathAuthFunc(s.PrivateKeyPathAuth)
+		if err != nil {
+			return nil, err
+		}
+		config.Auth = []gossh.AuthMethod{authFunc}
 	}
-	return config
+	return config, nil
 }
 func (s *Server) GetCmdClient() (*gossh.Client, error) {
-	config := s.GetConfig()
+	config, err := s.GetConfig()
+	if err != nil {
+		return nil, err
+	}
 	return gossh.Dial("tcp", fmt.Sprintf("%s:%s", s.Host, s.Port), config)
 }
 
 func (s *Server) GetScpClient() (*scp.SCP, error) {
-	config := s.GetConfig()
+	config, err := s.GetConfig()
+	if err != nil {
+		return nil, err
+	}
 	return scp.New(fmt.Sprintf("%s:%s", s.Host, s.Port), config)
 }
 
-func PublicKeyPathAuthFunc(publicKeyPath string) gossh.AuthMethod {
+func PublicKeyPathAuthFunc(publicKeyPath string) (gossh.AuthMethod, error) {
 	keyPath, err := homedir.Expand(publicKeyPath)
 	if err != nil {
 		zap.S().Fatal("find key's home dir failed", err)
+		return nil, err
 	}
 	key, err := os.ReadFile(keyPath)
 	if err != nil {
 		zap.S().Fatal("ssh key file read failed", err)
+		return nil, err
 	}
 	// Create the Signer for this private key.
 	signer, err := gossh.ParsePrivateKey(key)
 	if err != nil {
 		zap.S().Fatal("ssh key signer failed", err)
+		return nil, err
 	}
-	return gossh.PublicKeys(signer)
+	return gossh.PublicKeys(signer), nil
 }
 
-func PublicKeyAuthFunc(privateKey string) gossh.AuthMethod {
+func PublicKeyAuthFunc(privateKey string) (gossh.AuthMethod, error) {
 	signer, err := gossh.ParsePrivateKey([]byte(privateKey))
 	if err != nil {
 		zap.S().Fatal("ssh key signer failed", err)
+		return nil, err
 	}
-	return gossh.PublicKeys(signer)
+	return gossh.PublicKeys(signer), nil
 }
